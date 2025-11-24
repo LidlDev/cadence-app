ef5f0f732bd297eef0ab82f3ddd0355c26baccf4
Harry <contactusspike@gmail.com>
2025-11-24 14:48:57 +1100

    Update DashboardClient.tsx
    Updating personal bests to pass runs

--- components/dashboard/DashboardClient.tsx
+++ components/dashboard/DashboardClient.tsx
@@ -43,7 +43,7 @@ export default function DashboardClient({
           </div>
 
           {/* Personal Bests */}
-          <PersonalBestsCard personalBests={personalBests} />
+          <PersonalBestsCard personalBests={personalBests} runs={runs} />
 
           {/* Performance Predictions */}
           <PredictionsCard />
